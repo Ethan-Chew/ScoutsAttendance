d83f487b8ba5aa5fe53a3bc02234e76535ce6591
Ethan Chew <ethanchewmh@gmail.com>
2021-05-14 18:36:48 +0800

    Initial Commit

---
+++ ScoutsAttendance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoutsAttendance
+//
+//  Created by Ethan Chew on 14/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
