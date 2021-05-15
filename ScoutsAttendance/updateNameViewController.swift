d9e6e6b3f4e39efeb174001c0cb2bfd8afc12f23
Ethan Chew <ethanchewmh@gmail.com>
2021-05-15 13:33:50 +0800

    Constrains

---
+++ ScoutsAttendance/updateNameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  updateNameViewController.swift
+//  ScoutsAttendance
+//
+//  Created by Ethan Chew on 14/5/21.
+//
+
+import UIKit
+
+class updateNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
